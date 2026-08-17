abe4fbe1e4fe68e3ffe9dfaa8e20262f022573bf
Lee Ee Xuan <e1525562.u.nus.edu>
2026-08-17 10:21:08 +0800

    Added chatbot greetings

--- src/main/java/CLANKER.java
+++ src/main/java/CLANKER.java
@@ -1,5 +1,6 @@
 public class CLANKER {
     public static void main(String[] args) {
+        String seperator = "─".repeat(60);
         String banner = """
  _____   _          _      _   _  _  __  _____  _____
 /  ___| | |        / \\    | \\ | || |/ / | ____||  _  \\
@@ -7,6 +8,16 @@ public class CLANKER {
 | |___  | |___   / ___ \\  | |\\  ||  <   | |___ |  _ <
 \\_____| |_____| /_/   \\_\\ |_| \\_||_|\\_\\ |_____||_| \\_\\
 """;
+        String greeting = """
+I'm CLANKER.
+What do you want?
+""";
+        String goodbye = "Ok bye.";
+
+        System.out.println(seperator);
         System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(seperator);
+        System.out.println(goodbye);
     }
 }
